0997f19ecf9cbfb7afe48589962da6f339d27da8
vagrant <vagrant@precise32.(none)>
2014-02-07 23:48:53 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,15 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$("a.nameListener").click(nameClick);
+}
+
+function nameClick(e) {
+	console.log("Name clicked");
+	e.preventDefault();
+
+	var currentName = $(this).text();
+	$(this).text(anagrammedName(currentName));
 }
 
 function anagrammedName(name) {
@@ -43,4 +52,4 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
-}
+
